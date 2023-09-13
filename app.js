990e30c495ff6c3e67903906175115374a735dce
Charles2205 <cewudzie0@gmail.com>
2023-09-13 12:17:35 +0000

    Using Brevo API

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const nodemailer = require('nodemailer')
 
 const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    host: "smtp-relay.brevo.com",
+    port: 587,
     auth: {
-      user: "bf1da955ebf9d6",
-      pass: "e95ee819e78047"
+      user: "cewudzie0@gmail.com",
+      pass: "ORPptmDEXnhM5cKQ"
     }
 });
 
 const sendEmail =async()=>{
     await transport.sendMail({
         to:'cewudzie0@gmail.com',
-        from: 'abc@gmail.com',
+        from: 'trialbethel0@gmail.com',
         subject:'Testing mailer',
         html:"<h1>hello I'm still testing email </h1>"
     })
