be115825c28f690f15e9f68b4793e56ecdd9c58b
Charles2205 <cewudzie0@gmail.com>
2023-09-18 12:38:16 +0000

    configured web server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotEnv = require('dotenv').config()
+const {adminRoute,frontendRoute} = require('./routes')
+
+const PORT = process.env.PORT
+const app =express()
+
+
+
+
+app.use('/admin',adminRoute)
+app.use('/',frontendRoute )
+
+
+
+
+const startServer=async()=>{
+try {
+    app.listen(PORT,()=>{
+        console.log(`App is running on http://localhost:${PORT}`);
+    })
+} catch (error) {
+    console.log(Error);
+}
+} 
+startServer()
