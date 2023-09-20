fef073fd9d1b2bb5ab13e4ff84c61cf144426bfb
Charles2205 <cewudzie0@gmail.com>
2023-09-20 13:16:07 +0000

    Update: Added Admin Login Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,11 @@
 const index =(req,res)=>{
 res.render('pages/admin/pages/index')
 }
-
+const viewLogin =(req,res)=>{
+    res.render('pages/admin/pages/login',{layout:'pages/admin/layouts/other'})
+}
 
 module.exports={
-    index
+    index,
+    viewLogin
 }
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,6 @@ const express = require('express')
 const app =express.Router()
 
 app.get('/', adminController.index)
+app.get('/login',adminController.viewLogin)
 
 module.exports=app
