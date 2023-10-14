d0dd1a79a3495dba774cf52ca1c1a05a3115b2fe
Charles2205 <cewudzie0@gmail.com>
2023-10-14 07:58:25 +0000

    Update Admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const {adminController}=require('../controllers')
 const express = require('express')
 const app =express.Router()
 
-app.get('/', adminController.index)
-app.get('/login',adminController.viewLogin)
+app.get('/', adminController.viewLogin)
+app.get('/',adminController.index)
 
 module.exports=app
