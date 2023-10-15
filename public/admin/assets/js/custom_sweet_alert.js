74ed2f4c9e5fd34b8edce93eb65b4a1feb631ae6
Charles2205 <cewudzie0@gmail.com>
2023-10-15 15:02:09 +0000

    Updatre

--- public/admin/assets/js/custom_sweet_alert.js
+++ public/admin/assets/js/custom_sweet_alert.js
@@ -1,37 +1,37 @@
 const btnSave = document.querySelector("#btnSave")
 btnSave.addEventListener ("click",async(e)=>{
-e.preventDefault()
+    e.preventDefault()
 
-let allInputs = document.querySelectorAll(".inputValidator")
-allInputs = Array.from(allInputs)
-const result = allInputs.filter((input)=>{
-    if(input.value.length<1){
-        showToast("error","field required")
-        return false
-    }
-    return true 
-})
-console.log(result); 
-
-
-
-if(result.length>2){
-    const userName = document.querySelector("#user_name").value
-    const password = document.querySelector("#password").value
-
-    const res = await axios.get("/") .then(response => {
-      // Assuming a successful response from the server
-      window.location.href = '/'; // Redirect to the desired page
+    let allInputs = document.querySelectorAll(".inputValidator")
+    allInputs = Array.from(allInputs)
+    const result = allInputs.filter((input)=>{
+        if(input.value.length<1){
+            showToast("error","field required")
+            return false
+        }
+        return true 
     })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-    console.log(res);
-   }
+    if(result.length>2){
+        // const userName = document.querySelector("#user_name").value
+        // const password = document.querySelector("#password").value
+    
+        // const res = await axios.get("/") .then(response => {
+        //     // showToast('success','Login Success')
+        //   // Assuming a successful response from the server
+        //   window.location.href = '/'; // Redirect to the desired page
+        // })
+        // .catch(error => {
+        //   console.error('Error:', error);
+        // });
+        // console.log(res);
+        alert('Hello')
+    }
+    
+    // console.log(result); 
 
-   
+    
 
-})
+    })
 
 
 
