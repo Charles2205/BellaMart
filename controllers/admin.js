ffff73dc9195f38893de7bec86841cc0d635d57e
Charles2205 <cewudzie0@gmail.com>
2023-11-19 22:49:42 +0000

    update

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,7 +12,7 @@ const createAdmin=async(req,res)=>{
     const {fullName,email,password,phoneNumber,role}= req.body
 try {
    await dbAdmin.create({fullName,email,password,phoneNumber,role})
-   console.log('Admin created Successfully');
+   console.log('Admin created Successfully yes');
     res.redirect("/admin/auth")
     
 } catch (error) {
